7054814faa9ba2ca4237270d8b3f146699fd0e15
Natalia <166624923+natssj@users.noreply.github.com>
2026-09-18 16:06:31 +0200

    Update recibe_numero.c
    Los nodos nuevos dejan de ponerse al principio
    En el main se diferencia el stack A del B

--- recibe_numero.c
+++ recibe_numero.c
@@ -19,27 +19,55 @@ t_node *new_node(int value)
     return (node);
 }
  
-void add_node(t_node **head, t_node *new)
+// void add_node(t_node **head, t_node *new)
+// {
+//     new->next = *head;
+//     *head = new;
+// }
+
+void add_node_last(t_node **head, t_node *new)
 {
-    new->next = *head;
-    *head = new;
+	t_node	*last;
+
+	if (!new)
+		return ;
+	if (!*head)
+	{
+		*head = new; //sirve para la primera llamada (en el main inicializo con head = NULL)
+		return ;
+	}
+	last = *head;
+	while(last->next)
+		last = last->next;
+    last->next = new;
 }
  
 int main(int argc, char **argv)
 {
-    t_node *head;
+    t_node *head_a;
+	t_node *head_b;
     t_node *tmp;
     int     i;
  
-    head = NULL; //la lista empieza vacia
+    head_a = NULL; //la lista empieza vacia
+	head_b = NULL;
     i = 1; //se empieza a leer desde argv 1 (no cuenta el nombre del programa)
     while (i < argc)
     {
         tmp = new_node(atoi(argv[i]));
-        add_node(&head, tmp);
+        add_node_last(&head_a, tmp);
         i++;
     }
-    tmp = head;
+    tmp = head_a;
+	printf("Stack A:\n");
+    while (tmp)
+    {
+		
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+	tmp = head_b;
+	printf("Stack B:\n");
     while (tmp)
     {
         printf("%d\n", tmp->value);
